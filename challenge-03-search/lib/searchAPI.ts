b48720fc06b11e26755d757e2f0515620b3004fb
gustavo brun <gustavobdossantos.gb@gmail.com>
2025-08-26 20:36:19 -0300

    refactor: refactor third challenge algorithm

--- challenge-03-search/lib/searchAPI.ts
+++ challenge-03-search/lib/searchAPI.ts
@@ -19,6 +19,57 @@ export async function searchKnowledge(query: string, limit = 10): Promise<Search
 		return []
 	}
 
+	// IDF calc
+	const totalDocs = mockResults.length
+	const docsWithQuery = mockResults.filter((result) => {
+		return Object.values(result).some((value) => {
+			const treatedValue = String(value).toLowerCase()
+
+			return treatedValue.includes(query.toLowerCase())
+		})
+	}).length
+
+	const idf = Math.log(totalDocs / docsWithQuery)
+
+	const treatedMockResults = mockResults.map((result) => {
+		// ['1', 'implementação', 'de', 'rag', 'na', ...][]
+		const tokenizedResult = Object.values(result).join(' ').toLowerCase().split(' ')
+
+		// TF calc
+		const appearances = tokenizedResult.reduce((acc, word) => {
+			if (word === query) {
+				return acc + 1
+			}
+			return acc
+		})
+		const totalResults = tokenizedResult.length
+		const tf = Number(appearances) / totalResults
+
+		const score = tf * idf
+
+		return {
+			result,
+			tf,
+			score,
+		}
+	})
+
+	const sorted = treatedMockResults.sort((a, b) => b.score - a.score).slice(0, limit)
+
+	console.log({ sorted, idf, docsWithQuery, totalDocs })
+
+	const payload = sorted.map((treatedMockResult) => {
+		return { ...treatedMockResult.result, score: treatedMockResult.score }
+	})
+
+	return payload
+}
+
+export async function searchKnowlsedge(query: string, limit = 10): Promise<SearchResult[]> {
+	if (!query.trim()) {
+		return []
+	}
+
 	// PROBLEMA 1: Só faz includes(), sem algoritmo de relevância
 	const filtered = mockResults.filter((result) => {
 		const searchTerm = query.toLowerCase()
